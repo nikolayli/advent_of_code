fa8a5cdb28aecf218a0c9d32bff7f73fb67e310b
m4k1m4 <m4k1m4fun@gmail.com>
2024-02-21 17:50:35 +0300

    Advent of Code

--- 2016/day1.js
+++ 2016/day1.js
@@ -25,6 +25,50 @@ function partOne(instructions) {
   return Math.abs(x) + Math.abs(y);
 }
 
+function partTwo(instructions) {
+  let x = 0;
+  let y = 0;
+  let direction ='N';
+  let visited = new Set();
+  let firstLocationVisitedTwice = null;
+
+  const directions = {
+    N: { L: 'W', R: 'E', x: 0, y: 1},
+    E: { L: 'N', R: 'S', x: 1, y: 0},
+    S: { L: 'E', R: 'W', x: 0, y: -1},
+    W: { L: 'S', R: 'N', x: -1, y: 0},
+  };
+
+  function updateDirection(turn) {
+    direction = directions[direction][turn];
+  }
+
+  function moveForward(blocks) {
+    for (let i = 0; i < blocks; ++i) {
+      x += directions[direction].x;
+      y += directions[direction].y;
+
+      const location = `${x}, ${y}`;
+      if (visited.has(location) && firstLocationVisitedTwice === null) {
+        firstLocationVisitedTwice = Math.abs(x) + Math.abs(y);
+      } else {
+        visited.add(location);
+      }
+    }
+  }
+  
+  instructions.forEach(instruction => {
+    const turn = instruction.charAt(0);
+    const blocks = parseInt(instruction.slice(1));
+
+    updateDirection(turn);
+    moveForward(blocks);
+  });
+
+  return firstLocationVisitedTwice;
+
+}
+
 fs.readFile('input.txt', 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading file:', err);
@@ -33,5 +77,9 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
   const instructions = data.trim().split(', ');
 
   const shortestPath = partOne(instructions);
+  const firstLocationVisitedTwice = partTwo(instructions);
+
   console.log('Shortest way:', shortestPath);
-})
+  console.log('First location visited twice:', firstLocationVisitedTwice);
+
+});
